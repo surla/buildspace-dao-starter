b03daae0567960b6f84e53aed173df14bbedf647
Joseph Surla <josephsurla@gmail.com>
2021-12-23 22:13:09 -0800

    import Thirdweb and add wallet connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useMemo, useState } from "react";
+
+// Import thirdweb
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("👋 Address:", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>🍙 Welcome to OnigiriDAO</h1>
+        <button onClick={() => connectWallet("injected")} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>👀 wallet connected, now what!</h1>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,27 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App.jsx";
 
+// Import ThirdWeb
+import { ThirdwebWeb3Provider } from "@3rdweb/hooks";
+
+// Include Rinkby chain support
+// 4 = Rinkby
+const supportedChainIds = [4];
+
+// Add metamask wallet support
+const connectors = {
+  injected: {},
+};
+
 // Render the App component to the DOM
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThirdwebWeb3Provider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <App />
+    </ThirdwebWeb3Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
